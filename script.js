2e86bd06b90559bda07d2b2ad0c42f05321af9a4
Lorenzo Bianchi <lorenzo.bianchi0210@gmail.com>
2022-10-30 02:43:58 -0300

    Get public repos

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 const octokit = new Octokit({
-    auth: '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'
+    auth: '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'
   });
 
-  const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
-  console.log("Hello, %s", login);
+const {
+  data: { login },
+} = await octokit.rest.users.getAuthenticated();
+console.log("Hello, %s", login);
+
+const repos = await octokit.request('GET /users/{username}/repos', {
+  username: 'LBianchi98'
+});
+console.log(repos.data);
